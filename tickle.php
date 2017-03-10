5a047a2f2b57e028b096a5c94869a73d1bee6366
killscreen <killscreen@users.noreply.github.com>
2017-03-10 15:56:01 -0600

    accept length argument

--- tickle.php
+++ tickle.php
@@ -1,9 +1,9 @@
 <?php
 require 'markov.php';
 
-$order = 4;
-$length = 6;
+$order = 6;
+$length = isset($_GET["l"]) ? $_GET["l"] : 128;
 $file = "./tickle.txt";
   
-echo generate_markov_text($length, generate_markov_table(file_get_contents($file), $order), $order);
+echo generate_markov_text($length, generate_markov_table(file_get_contents($file), $order), $order / 2);
 ?>
