aff3c373d2327f097252b2bb93019df03a6eb34f
killscreen <killscreen@users.noreply.github.com>
2017-03-10 16:41:07 -0600

    restrict inputs

--- tickle.php
+++ tickle.php
@@ -1,8 +1,8 @@
 <?php
 require 'markov.php';
 
-$order = isset($_GET["o"]) ? $_GET["o"] : 3;
-$length = isset($_GET["l"]) ? $_GET["l"] : 128;
+$order = min(max(isset($_GET["o"]) && is_numeric($_GET["o"]) ? $_GET["o"] : 3, 1), 10);
+$length = min(max(isset($_GET["l"]) && is_numeric($_GET["l"]) ? $_GET["l"] : 128, 1), 256);
 $file = "./tickle.txt";
   
 echo trim(trim(generate_markov_text($length, generate_markov_table(file_get_contents($file), $order * 2), $order), 'a..z'));
