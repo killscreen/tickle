173b1f3a096c32037601ed39ccfca06a144231f6
killscreen <killscreen@users.noreply.github.com>
2017-03-10 16:56:44 -0600

    pass in element to update

--- tickle.js
+++ tickle.js
@@ -1,14 +1,14 @@
-function update() {
-  var text = this.responseText;
+function update(element, response) {
+  var text = response.target.responseText;
   var span = document.createElement('span');
   span.textContent = text;
-  while (document.body.firstChild) document.body.removeChild(document.body.firstChild);
-  document.body.appendChild(span);
+  while (element.firstChild) element.removeChild(element.firstChild);
+  element.appendChild(span);
 }
 
 function main() {
   var xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", update);
+  xhr.addEventListener("load", update.bind(null, document.body));
   xhr.addEventListener("load", setTimeout.bind(window, main, 1000));
   xhr.open("GET", "tickle.php");
   xhr.send();
