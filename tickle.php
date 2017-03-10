e38ae2102efbb28bd66fd3ca159932c6120e2f75
killscreen <killscreen@users.noreply.github.com>
2017-03-10 16:21:53 -0600

    Trim whitespace too

--- tickle.php
+++ tickle.php
@@ -5,5 +5,5 @@ $order = isset($_GET["o"]) ? $_GET["o"] : 3;
 $length = isset($_GET["l"]) ? $_GET["l"] : 128;
 $file = "./tickle.txt";
   
-echo trim(generate_markov_text($length, generate_markov_table(file_get_contents($file), $order * 2), $order), 'a..z');
+echo trim(trim(generate_markov_text($length, generate_markov_table(file_get_contents($file), $order * 2), $order), 'a..z'));
 ?>
