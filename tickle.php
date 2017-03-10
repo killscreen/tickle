6ecc9b88430f660c9f626dcab7bfbca97ca5f2a7
killscreen <killscreen@users.noreply.github.com>
2017-03-10 15:58:58 -0600

    Allow order to be specified

--- tickle.php
+++ tickle.php
@@ -1,9 +1,9 @@
 <?php
 require 'markov.php';
 
-$order = 6;
+$order = isset($_GET["o"]) ? $_GET["o"] : 3;
 $length = isset($_GET["l"]) ? $_GET["l"] : 128;
 $file = "./tickle.txt";
   
-echo generate_markov_text($length, generate_markov_table(file_get_contents($file), $order), $order / 2);
+echo generate_markov_text($length, generate_markov_table(file_get_contents($file), $order * 2), $order);
 ?>
