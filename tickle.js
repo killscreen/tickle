ed99cf88b7a32694435c33446248290c13224a26
killscreen <killscreen@users.noreply.github.com>
2017-03-10 16:51:53 -0600

    Load poetry

--- tickle.js
+++ tickle.js
@@ -1,5 +1,16 @@
+function update() {
+  var text = this.responseText;
+  var span = document.createElement('span');
+  span.textContent = text;
+  document.body.appendChild(span);
+}
+
 function main() {
-  alert('woo');
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", update);
+  xhr.addEventListener("load", setTimeout.bind(window, main, 1000));
+  xhr.open("GET", "tickle.php");
+  xhr.send();
 }
 
 document.addEventListener("DOMContentLoaded", main)
