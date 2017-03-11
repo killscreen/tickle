2c0ca04f4396847889b18cae40d98e518be9884b
killscreen <killscreen@users.noreply.github.com>
2017-03-10 19:04:56 -0600

    Use callbacks to keep it moving

--- tickle.js
+++ tickle.js
@@ -1,17 +1,18 @@
 function utter(text, callback) {
   var utterance = new SpeechSynthesisUtterance(text.split('\n').join('. '));
   var voices = speechSynthesis.getVoices();
-  utterance.pitch = Math.sin(Date.now() / 600) + 1;
-  utterance.rate = Math.sin(Date.now() / 3400) + 1.25;
+  utterance.pitch = Math.sin(Date.now() / 600) / 2 + 0.75;
+  utterance.rate = Math.sin(Date.now() / 3400) / 2 + 1;
   utterance.volume = Math.sin(Date.now() / 9000) / 2 + 1;
   utterance.voice = voices[Math.floor(Date.now() / 60000) % voices.length];
   utterance.onend = callback;
   speechSynthesis.speak(utterance);
 }
 
-function evict(element) {
+function evict(callback, element) {
   element.className = "removing";
   element.addEventListener("transitionend", element.remove.bind(element));
+  callback();
 }
 
 function mark(element) {
@@ -24,12 +25,12 @@ function append(line) {
   this.appendChild(p);
 }
 
-function update(element, response) {
+function update(callback, element, response) {
   var text = response.target.responseText;
   var div = document.createElement('div');
   element.appendChild(div);
   text.split('\n').forEach(append.bind(div));
-  utter(text, evict.bind(null, div));
+  utter(text, evict.bind(null, callback, div));
   setTimeout(mark.bind(null, div), 100);
 }
 
@@ -37,8 +38,7 @@ function main() {
   var len = Math.sin(Date.now() / 3000) * 20 + 28;
   var order = Math.sin(Date.now() / 1700) * 24 + 26;  
   var xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", update.bind(null, document.body));
-  xhr.addEventListener("load", setTimeout.bind(window, main, 150 * len - 200));
+  xhr.addEventListener("load", update.bind(null, main, document.body));
   xhr.open("GET", "tickle.php?l=" + len);
   xhr.send();
 }
