b6958f0b41fc06efc2743b2292d8d1cb874eb484
killscreen <killscreen@users.noreply.github.com>
2017-03-10 18:07:49 -0600

    play with length, timing

--- tickle.js
+++ tickle.js
@@ -18,15 +18,16 @@ function update(element, response) {
   span.textContent = text;
   element.appendChild(span);
   setTimeout(utter.bind(null, text), 0);
-  setTimeout(mark.bind(null, span), 0);
-  setTimeout(evict.bind(null, span), 8000);
+  setTimeout(mark.bind(null, span), 100);
+  setTimeout(evict.bind(null, span), 50 * text.length);
 }
 
 function main() {
+  var len = Math.sin(Date.now() / 3000) * 40 + 50;
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("load", update.bind(null, document.body));
-  xhr.addEventListener("load", setTimeout.bind(window, main, 12000));
-  xhr.open("GET", "tickle.php");
+  xhr.addEventListener("load", setTimeout.bind(window, main, 120 * len));
+  xhr.open("GET", "tickle.php?l=" + len);
   xhr.send();
 }
 
